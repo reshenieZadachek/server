c3f00cb43330162563d65fc6db461baabeacb8c0
reshenieZadachek <158518714+reshenieZadachek@users.noreply.github.com>
2024-05-19 09:05:55 +0300

    Update roomRouter.js

--- routes/roomRouter.js
+++ routes/roomRouter.js
@@ -4,10 +4,6 @@ const RoomController = require('../controllers/roomController')
 
 
 router.get('/my', RoomController.getMy)
-router.get('/TESTROOMS', RoomController.testSetRoom)
-router.get('/TESTUSERS', RoomController.testSetUsers)
-router.get('/TESTDELETE', RoomController.testDELETE)
-router.get('/testGETSECROOM', RoomController.testGETSECROOM)
 router.post('/join', RoomController.joinRoom)
 
 
